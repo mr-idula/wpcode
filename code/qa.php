c40a64de30cdb2294a2665e56883c0d41034e36d
mr-idula <112497378+mr-idula@users.noreply.github.com>
2023-06-06 12:19:46 +0530

    Create qa.php

---
+++ code/qa.php
@@ -0,0 +1,72 @@
+<html>
+<head>
+<title>Q&A</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+<style>
+    #entire{
+        margin: 50px;
+    }
+    .title{
+       font-family: Georgia; 
+       font-size: 11pt;
+    }
+    #body{
+        text-align: center;
+        background-color: lavender;
+        font-family: Georgia;
+    }
+    #img1{
+        height: 200px;
+        width: 200px; 
+    }
+    .container{
+        background-color: white;
+        width: 10000px;
+        height: 600px;
+        font-family: Georgia;
+    }
+</style>
+</head>
+
+<body>
+<div id="entire">
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm"style="font-family: Georgia;">
+        <h5 class="my-0 mr-md-auto font-weight-normal">Neighborhood Gems</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+          <a class="p-2 text-dark" href="home.php">Home</a>
+          <a class="p-2 text-dark" href="about.php">About</a>
+          <a class="p-2 text-dark" href="qa.php">Q & A</a>
+          <a class="p-2 text-dark" href="cl.php">Checklist</a>
+        </nav>
+        <a href="login.php" class="btn btn-outline-info mr-1" role="button">Login</a>
+      </div>
+    <div class="container">
+    <div id= body class="jumbotron">
+      <h1> Site Description </h1>
+      <div class="card">
+        <div class="card-body" style="background-color: lightblue;">
+         <p> Q: Who are the users of the site? </p>
+          <p>A: The users of the site are people visiting, living, or interested in visiting in Atlanta and want to find & support local businesses. 
+        </p>
+           <p> Q: What will users get out of using this site?</p>
+        <p> A: Users with a membership will be able to discover local businesses within Atlanta. Users can also post local businesses to the forum. 
+        </p>
+        <p> Q: What problem does this website solve? </p>
+      <p>A: As of now, there is not a site that is dedicated to advertising and highlighting local businesses in Atlanta. This website solves this problem
+          by listing and categorizing local businesses. 
+        </p>
+        <p> Q: What is the end goal of this website? </p>
+      <p> A: Once users access and utilizie my site, I hope they visit and support the amazing local businesses listed. I also hope they make an effort to sh$
+          local and realize the importance of doing so. 
+        </p>
+        </div>
+      </div>
+    </div>
+    </div>
+</body>
+  
