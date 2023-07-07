495d1955d03eed0d8aef176daa77dc58c87a282e
mr-idula <112497378+mr-idula@users.noreply.github.com>
2023-07-07 16:41:12 +0530

    Create ls.php

---
+++ code/ls.php
@@ -0,0 +1,380 @@
+<html>
+<head>
+<title> Lifestyle </title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.3/dist/jquery.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+<style>
+    #entire{
+        margin: 50px;
+    }
+    .title{
+       font-family: Georgia; 
+       font-size: 11pt;
+    }
+    #body{
+        text-align: center;
+        background-color: lavender;
+        font-family: Georgia; 
+    }
+    #img1{
+        height: 200px;
+        width: 200x; 
+    }
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+</style>
+</head>
+
+<body>
+  <div id="entire">
+    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm" style="font-family: Georgia;">
+        <h5 class="my-0 mr-md-auto font-weight-normal" >Neighborhood Gems</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+          <a class="p-2 text-dark" href="home.php">Home</a>
+          <a class="p-2 text-dark" href="about.php">About</a>
+          <a class="p-2 text-dark" href="qa.php">Q & A</a>
+          <a class="p-2 text-dark" href="cl.php">Checklist</a>
+        </nav>
+        <div class="dropdown" style="text-align: right;">
+            <div class="btn-group">
+              <a href="lb.php" class="btn btn-outline-info" role="button">Local Businesses</a>
+                <button type="button" class="btn btn-outline-info dropdown-toggle dropdown-toggle-split mr-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <span class="sr-only">Toggle Dropdown</span>
+                </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="fd.php">Food & Drink</a>
+              <a class="dropdown-item" href="nl.php">Nightlife</a>
+              <a class="dropdown-item" href="cg.php#">Clothing</a>
+              <a class="dropdown-item" href="ls.php">Lifestyle</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Forum</a>
+            </div>
+            </div>
+          </div>
+        <a href="home.php" class="btn btn-outline-info mr-1" role="button">Log Out</a>
+      </div>
+      <main>
+
+        <section class="py-5 text-center container" style="background-color: lavender; font-family: Georgia; height: 500px;">
+          <div class="row py-lg-5">
+            <div class="col-lg-6 col-md-8 mx-auto">
+              <h1 class="fw-light">Lifestyle</h1>
+              <p class="lead text-muted"> Here are some fun and ecletic places to explore! <br>
+            </p>
+              <img id="img1" src="https://media.giphy.com/media/IWon6VgzVwEnu/giphy.gif">
+              <p> Doug the dog is getting ready to visit these places & you should too! <br><br>
+                  Scroll to the bottom of the page to see the location of each listing. Hover your cursor over the marker to see which lisiting it is. 
+              </p>
+            </div>
+          </div>
+        </section>
+      
+        <div class="album py-5" style="background-color: lightblue; font-family: Georgia; padding-top: 30px; margin: 50px;">
+          <div class="container">
+      
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                <div class="col" style="padding-top: 30px ;">
+                <div class="card shadow-sm">
+                  <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+                    <image href="https://cdn.shopify.com/s/files/1/0092/4227/9972/products/pumice_12qt_01_2048x2048.jpg?v=1637857023" width="100%" height="auto"/>
+                </svg>
+                  <div class="card-body">
+                    <p class="card-text"><b>The Victorian</b><br>
+                      Creative and cute plant store that also has a coffee shop. They have a wide range of plants with
+                      prices ranging from $10 - $500. They also offer potting services and have botanical experts ready to 
+                      help out. 
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="https://thevictorianatlanta.com" class= "btn btn-outline-info mr-1" role="button">More Info</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col" style="padding-top: 30px ;">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+                        <image href="https://images.squarespace-cdn.com/content/v1/59a20a42f43b558cac6ab4cf/1599230004223-WFZ22ZJLB9SRSJ1SZ85Z/merchant.jpg"  width="100%"  height="auto" />
+                    </svg>
+                  <div class="card-body">
+                    <p class="card-text"><b>The Merchant</b><br>
+                        Located in Krog Street Market, this stationery and gift store offers a range of ecletic items. <br><br>
+
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="https://thevictorianatlanta.com" class= "btn btn-outline-info mr-1" role="button">More Info</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col" style="padding-top: 30px ;">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+                        <image href="https://cdn.dribbble.com/users/1864037/screenshots/4468486/image.png"  width="100%"  height="auto" />
+                    </svg>
+                  <div class="card-body">
+                    <p class="card-text"><b>Studio 219 Ink</b><br>
+                    Feeling adventurous and don't mind a little pain? Studio 219 Ink is home to mulitple
+                    tattoo artists and piercers. They offer special prices every Tuesday, Wednesday, Thursday,
+                    and Saturday!  
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="https://www.studio219ink.com" class= "btn btn-outline-info mr-1" role="button">More Info</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+      
+              <div class="col" style="padding-top: 30px ;">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="auto" />
+                        <image href="https://res.cloudinary.com/simpleview/image/upload/w_1100,h_560,c_fill,g_auto/crm/atlanta/Criminal_Records_2.jpg0-1f6c4af05056a36_1f6c4ce9-5056-a36a-0a166f7cf611c6c8.jpg" width="auto" height="225" />
+                    </svg>
+                  <div class="card-body">
+                    <p class="card-text"><b>Criminal Records</b><br>
+                    Offers a large variety of records, comics, figurines, and posters. They also carry special edition
+                    records and you can sell old records! 
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="https://www.studio219ink.com" class= "btn btn-outline-info mr-1" role="button">More Info</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col" style="padding-top: 30px ;">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+                        <image href="https://secretatlanta.co/wp-content/uploads/2022/10/NB-e1666808896618.jpeg" width="100%" height="auto" />
+                    </svg>
+                  <div class="card-body">
+                    <p class="card-text"><b>The Green Flamingo</b><br>
+                    This Virgina Highland establishment offers a variety of indoor plants, offers potting services, 
+                    and the staff is well versed in botanical knowledge. It's also located on a strip of stores, making it 
+                    perfect for a Saturday afternoon shopping adventure!
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="https://www.instagram.com/thegreenflamingoatl/?hl=en" class= "btn btn-outline-info mr-1" role="button">More Info</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col" style="padding-top: 30px ;">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+                        <image href="https://thegavoice.com/wp-content/uploads/2019/09/Starlight-Drive-In-750x400-092719.jpg" width="auto" height="225" />
+                    </svg>
+                  <div class="card-body">
+                    <p class="card-text"><b>Starlight Drive-in </b><br>
+                        Take a step back in time and visit this drive in movie theatre. This establisment has been open 
+                        since 1956!
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="https://starlightdrivein.com" class= "btn btn-outline-info mr-1" role="button">More Info</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+      
+              <div class="col" style="padding-top: 30px ;">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+                        <image href="https://pbs.twimg.com/media/FdHeyYQaIAI1EuS.jpg" width="100%" height="auto" />
+                    </svg>
+                  <div class="card-body">
+                    <p class="card-text"><b>Atlanta Vintage Books</b><br>
+                        This establishment has been open for over 31 years and it contains over 75,000
+                        vintage, collectible, and used books. The staff is friendly, knowledgeable, and will 
+                        always help you find a book!
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="https://www.atlantavintagebooks.com" class= "btn btn-outline-info mr-1" role="button">More Info</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col" style="padding-top: 30px ;">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+                        <image href="https://cdn.shopify.com/s/files/1/0173/5264/files/beehive_exterior3_large.jpg?6213" width="100%" height="auto" />
+                    </svg>
+                  <div class="card-body">
+                    <p class="card-text"><b>The Beehive</b><br>
+                        A boutique collective comprised of independent designers with a focus on the local entrepreneur. 
+                        The product selection includes apparel, housewares, accessories, baby goods, local food goods and jewelry for everyone. 
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="https://thebeehiveatl.com" class= "btn btn-outline-info mr-1" role="button">More Info</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col" style="padding-top: 30px ;">
+                <div class="card shadow-sm">
+                    <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" />
+                        <image href="https://cdn.shopify.com/s/files/1/0263/9252/6947/files/121820_The_Print_Shop-09858_new_1400x.jpg?v=1614336800" width="100%" height="auto" />
+                    </svg>
+                  <div class="card-body">
+                    <p class="card-text" ><b>The Print Shop by FreeMarket</b><br>
+                        Focusing on approachable and affordable artwork offering limited edition, 
+                        exclusively-licensed art prints from contemporary-style artists, 
+                        including Diego Peñuela, Aesek, Sanithna and more.
+                    </p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <a href="https://theprintshopatl.com" class= "btn btn-outline-info mr-1" role="button">More Info</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      
+      </main> 
+      <div id="map" style="width:100%;height:400px;"></div>
+
+<script>
+    function initMap(){
+    //Map options 
+
+    var options={
+        zoom:10.5,
+        center:{lat:33.7488, lng:-84.3877}
+    }
+    // New map
+    var map = new google.maps.Map(document.getElementById('map'), options);
+
+    /*
+    // add marker
+
+    var marker= new google.maps.Marker({
+        position:{lat:42.4668,lng:-70.9495},
+        map:map
+    });
+
+    var infoWindow = new google.maps.InfoWindow({
+        content: '<h1>Lynn MA</h1>'
+    });
+    marker.addListener('click', function(){
+        infoWindow.open(map, marker);
+    });
+    */
+   //marker array
+   var markers = [
+    { //1
+        coords: {lat:33.77113889128514,lng:-84.36260819590275}, 
+        content:'<h1 style="font-family:Georgia;font-size: small;">The Victorian</h1>'
+    },
+    {
+        //2
+        coords: {lat:33.75746335894089, lng:-84.36422968835832}, 
+        content:'<h1 style="font-family:Georgia;font-size: small;">The Merchant</h1>'
+    },
+    { //3
+        coords: {lat:33.80945854912226,lng: -84.36619771802506},
+        content:'<h1 style="font-family:Georgia;font-size: small;">Studio 219 Ink</h1>'
+    },
+    { //4
+        coords: {lat: 33.7653522790464,lng:-84.34989853151856}, 
+        content:'<h1 style="font-family:Georgia;font-size: small;">Criminal Records</h1>'
+    },
+    { //5
+        coords: {lat:33.78339288694392,lng:-84.35482330453317} , 
+        content:'<h1 style="font-family:Georgia;font-size: small;">The Green Flamingo</h1>'
+    },
+    { //6
+        coords: {lat:33.70248764026825, lng:-84.34871801617692} ,
+        content:'<h1 style="font-family:Georgia;font-size: small;">Starlight Drive-in</h1>'
+    },
+    { //7
+        coords: {lat:33.86692544378532, lng:-84.309778804531},
+        content:'<h1 style="font-family:Georgia;font-size: small;">Atlanta Vintage Bookstore</h1>'
+    },
+    { //8
+        coords: {lat:33.758842487892494, lng:-84.34793043151882},
+        content:'<h1 style="font-family:Georgia;font-size: small;">The Beehive</h1>'
+    },
+    { //9
+        coords: {lat:33.77278425840239, lng:-84.36564859104085},
+        content:'<h1 style="font-family:Georgia;font-size: small;">The Print Shop by FreeMarket</h1>' 
+    }
+   ];
+   // Loop through markers
+   for(var i=0; i < markers.length;i++){
+        addMarker(markers[i]);
+   }
+   function addMarker(props){
+        var marker= new google.maps.Marker({
+        position:props.coords,
+        map:map
+    });
+    //check to see if there is content 
+    if(props.content){
+        var infoWindow = new google.maps.InfoWindow({
+        content:props.content
+    });
+    marker.addListener('mouseover', function(){
+        infoWindow.open(map, marker);
+        
+    });
+    }
+   }
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBx_dA2JmhLXEy-s81YoJ1gwaQjyqt5cfc &callback=initMap"></script>
+
+</body>
+</html>
+</div>
+</body>
